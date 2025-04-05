dd7f7b35a6b9f278892b2878cb6b87a4d7683bba
Fanis <thfanis649@gmail.com>
2025-04-05 17:11:13 +0300

    migrated quest and tests

---
+++ QuestSystem/Quest.cs
@@ -0,0 +1,111 @@
+namespace QuestSystem;
+
+/// <summary>
+/// Represents a quest with one or more <see cref="QuestStage"/>.
+/// </summary>
+public class Quest
+{
+    // Main Properties
+    public string Title {get; private set;}
+    public int Id {get; private set;}
+    
+    private readonly Queue<QuestStage> _stagesQueue =  new();
+    
+    // Getter Properties
+    public QuestStage CurrentStage => _stagesQueue.Peek();
+    public int StagesLeft => _stagesQueue.Count;
+    public bool IsCompleted => _stagesQueue.Count == 0;
+    
+
+    public Quest(int id, string title, params QuestStage[] stages)
+    {
+        //Arg checks
+        if (id <= 0) {
+            throw new ArgumentException("Invalid quest id", nameof(id));
+        }
+        if (string.IsNullOrEmpty(title)) {
+            throw new ArgumentException("Title cannot be empty", nameof(title));
+        }
+        Id = id;
+        Title = title;
+        
+        //Null checks
+        ArgumentNullException.ThrowIfNull(stages, nameof(stages));
+        foreach (var stage in stages)
+        {
+            ArgumentNullException.ThrowIfNull(stage, $"{nameof(stages)} must not be null");
+        }
+        
+        // Fill queue
+        foreach (var stage in stages)
+        {
+            _stagesQueue.Enqueue (stage);    
+        }
+    }
+
+    /// <summary>
+    /// Constructs a quest with one inclusive stage using the provided objectives
+    /// </summary>
+    /// <param name="id"> The id of the quest</param>
+    /// <param name="title"> The title of the quest</param>
+    /// <param name="isInclusiveStage"> True if the stage is inclusive, false if selective</param>
+    /// <param name="stageDescription">The description of the quest stage</param>
+    /// <param name="objectives"> The objectives for the sole stage</param>
+    /// <exception cref="ArgumentException"></exception>
+    public Quest(int id, string title,bool isInclusiveStage,
+        string stageDescription, params Objective[] objectives)
+    {
+        //Arg checks
+        if (id <= 0) {
+            throw new ArgumentException("Invalid quest id", nameof(id));
+        }
+        if (string.IsNullOrEmpty(title)) {
+            throw new ArgumentException("Title cannot be empty", nameof(title));
+        }
+        Id = id;
+        Title = title;
+        
+        //Null checks
+        ArgumentNullException.ThrowIfNull(objectives, nameof(objectives));
+        foreach (var objective in objectives)
+        {
+            ArgumentNullException.ThrowIfNull(objective, 
+                $"{nameof(objective)} must not be null");
+        }
+        
+        // Create a quest stage and add to queue
+        QuestStage stage;
+        if (isInclusiveStage)
+        {
+            stage = new QuestStageInclusive(stageDescription,objectives);
+            _stagesQueue.Enqueue(stage);
+            return;
+        }
+        stage = new QuestStageSelective(stageDescription,objectives);
+        _stagesQueue.Enqueue(stage);
+    }
+
+    /// <summary>
+    /// Attempts to progress the quest based on the given information.
+    /// </summary>
+    /// <param name="objProgressDto"> The DTO containing the action that just happened</param>
+    /// <exception cref="InvalidOperationException"></exception>
+    public void TryProgressQuest(ObjectiveProgressDto objProgressDto)
+    {
+        // A completed quest cannot be progressed
+        if(IsCompleted) return;
+        
+        //Try progress the current stage based on arg
+        var currentStage = _stagesQueue.Peek();
+        if (currentStage.IsCompleted) {
+            throw new InvalidOperationException("The current stage is already completed");
+        }
+        currentStage.TryProgressTask(objProgressDto);
+
+        // Stage not completed yet
+        if (!currentStage.IsCompleted) return;
+        
+        // Stage just completed
+        _stagesQueue.Dequeue(); //move to the next stage or finish
+    }
+}
---
+++ QuestSystemTests/QuestTests.cs
@@ -0,0 +1,182 @@
+using QuestSystem;
+
+namespace QuestSystemTests;
+
+public class QuestTests
+{
+    
+    // Dependencies
+    private readonly Objective _taskKill,_taskKillLarge,_taskGather;
+    private readonly QuestStage _questStage1,_questStage2;
+    
+    // SUT
+    private readonly Quest _quest;
+    
+    public QuestTests()
+    {
+        // Dependencies
+        _taskKill = new Objective(5,(int)TaskType.Kill);
+        _taskKillLarge = new Objective(10,(int)TaskType.Kill);
+        _taskGather = new Objective(3,(int)TaskType.Gather);
+        _questStage1 = new QuestStageInclusive("kill and gather",_taskKill, _taskGather);
+        _questStage2 = new QuestStageInclusive("kill many",_taskKillLarge);
+        
+        // SUT
+        _quest = new Quest(1,"myQuest",_questStage1,_questStage2);
+    }
+    
+    [Fact]
+    public void Quest_ShouldInitializeProperly()
+    {
+        // Assert
+        Assert.Equal(0,_taskKill.CurrValue);
+        Assert.False(_taskKill.IsCompleted);
+        Assert.Equal(0,_taskKillLarge.CurrValue);
+        Assert.False(_taskKillLarge.IsCompleted);
+        Assert.Equal(0,_taskGather.CurrValue);
+        Assert.False(_taskGather.IsCompleted);
+        
+        Assert.False(_questStage1.IsCompleted);
+        Assert.False(_questStage2.IsCompleted);
+        
+        // Quest assertions
+        Assert.NotNull(_quest);
+        Assert.Equal("myQuest",_quest.Title);
+        Assert.Equal(1,_quest.Id);
+        Assert.False(_quest.IsCompleted);
+        Assert.Equal(2, _quest.StagesLeft);
+        Assert.Equal(_questStage1, _quest.CurrentStage);
+        
+        //Alt COnstructor assertions
+        var quest2 = new Quest(3, "title", true,
+            "stageDescr", _taskGather,_taskKill);
+        Assert.NotNull(quest2);
+        Assert.Equal("title",quest2.Title);
+        Assert.Equal(3,quest2.Id);
+        Assert.False(quest2.IsCompleted);
+        Assert.Equal(1, quest2.StagesLeft);
+    }
+    
+    [Fact]
+    public void Quest_ShouldThrowExceptionForNullStages()
+    {
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => new Quest(1,"title",null));
+        Assert.Throws<ArgumentNullException>(() => new Quest(1,"title",true,"stageDescr",null));
+    }
+    
+    [Fact]
+    public void Quest_ShouldThrowExceptionForNullTitle()
+    {
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => new Quest(1,null,_questStage1));
+        Assert.Throws<ArgumentException>(() => new Quest(1,null,true,"stageDescr",_taskGather));
+    }
+    
+    [Fact]
+    public void Quest_ShouldThrowExceptionForEmptyTitle()
+    {
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => new Quest(1,"",_questStage1));
+        Assert.Throws<ArgumentException>(() => new Quest(1,"",true,"stageDescr",_taskGather));
+    }
+    
+    [Fact]
+    public void Quest_ShouldThrowExceptionForNegativeId()
+    {
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => new Quest(-1,"myQuest",_questStage1));
+        Assert.Throws<ArgumentException>(() => new Quest(-1,"myQuest",true,"stageDescr",_taskGather));
+    }
+    
+    [Fact]
+    public void Quest_ShouldThrowExceptionForZeroId()
+    {
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => new Quest(0,"myQuest",_questStage1));
+        Assert.Throws<ArgumentException>(() => new Quest(0,"myQuest",true,"stageDescr",_taskGather));
+    }
+    
+    [Fact]
+    public void Quest_ShouldThrowExceptionForNullStageInStages()
+    {
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => new Quest(1,"myQuest",new QuestStageInclusive("",null)));
+    }
+    
+    [Fact]
+    public void TryProgressQuest_ShouldProgressCurrentStage()
+    {
+        //Arrange
+        var objectiveProgressDto = new ObjectiveProgressDto((int)TaskType.Kill,3);
+        
+        // Act
+        _quest.TryProgressQuest(objectiveProgressDto);
+
+        // Assert
+        Assert.Equal(_questStage1, _quest.CurrentStage);
+        Assert.Equal(3,_taskKill.CurrValue);
+        Assert.False(_questStage1.IsCompleted);
+    }
+    
+    [Fact]
+    public void TryProgressQuest_ShouldMoveToNextStageAfterCompletion()
+    {
+        //Arrange
+        var taskProgressDto  = new ObjectiveProgressDto((int)TaskType.Kill,5);
+        var taskProgressDto2 = new ObjectiveProgressDto((int)TaskType.Gather,5);
+        
+        // Act
+        _quest.TryProgressQuest(taskProgressDto);
+        _quest.TryProgressQuest(taskProgressDto2);
+
+        // Assert
+        Assert.True(_questStage1.IsCompleted);
+        Assert.Equal(_questStage2, _quest.CurrentStage);
+        Assert.Equal(1, _quest.StagesLeft);
+    }
+    
+    [Fact]
+    public void TryProgressQuest_ShouldMarkQuestAsCompleted()
+    {
+        //Arrange
+        var taskProgressDto = new ObjectiveProgressDto((int)TaskType.Kill,5);
+        var taskProgressDto2 = new ObjectiveProgressDto((int)TaskType.Gather,5);
+        
+        // Act - Assert
+        _quest.TryProgressQuest(taskProgressDto);
+        Assert.Equal(_quest.CurrentStage,_questStage1);
+        Assert.False(_questStage1.IsCompleted);
+        
+        _quest.TryProgressQuest(taskProgressDto2);
+        Assert.True(_questStage1.IsCompleted);
+        Assert.Equal(_quest.CurrentStage,_questStage2);
+        Assert.False(_questStage2.IsCompleted);
+    }
+    
+    [Fact]
+    public void TryProgressQuest_ShouldNotProceedCompletedQuest()
+    {
+        //Complete the quest
+        var taskProgressDto = new ObjectiveProgressDto((int)TaskType.Kill,5);
+        var taskProgressDto2 = new ObjectiveProgressDto((int)TaskType.Gather,5);
+        _quest.TryProgressQuest(taskProgressDto);
+        Assert.False(_quest.CurrentStage.IsCompleted);
+        Assert.Equal(2, _quest.StagesLeft);
+        Assert.False(_quest.IsCompleted);
+        
+        _quest.TryProgressQuest(taskProgressDto2);
+        Assert.Equal(1, _quest.StagesLeft);
+        Assert.False(_quest.IsCompleted);
+        
+        var taskProgressDto3 = new ObjectiveProgressDto((int)TaskType.Kill,15);
+        _quest.TryProgressQuest(taskProgressDto3);
+        Assert.Equal(0, _quest.StagesLeft);
+        Assert.True(_quest.IsCompleted);
+        
+        //Try to proceed past the end - nothing happens
+        _quest.TryProgressQuest(taskProgressDto3);
+        Assert.Equal(0, _quest.StagesLeft);
+        Assert.True(_quest.IsCompleted);
+    }
+}
