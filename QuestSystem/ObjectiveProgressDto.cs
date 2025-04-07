d6f59807c5d3a8401d921fc709c525fb6c65e65a
Fanis <thfanis649@gmail.com>
2025-04-07 21:07:55 +0300

    added to string on dto and tests

--- QuestSystem/ObjectiveProgressDto.cs
+++ QuestSystem/ObjectiveProgressDto.cs
@@ -3,17 +3,26 @@ namespace QuestSystem;
 /// <summary>
 /// A data transfer object corresponding to progress in a specific task.
 /// </summary>
-public struct ObjectiveProgressDto
+public readonly struct ObjectiveProgressDto
 {
-    public int TaskTypeId { get; init; }
-    public int ProgressValue { get; init; }
+    public int TaskTypeId { get; }
+    public int ProgressValue { get; }
+    public int AssetId { get;}
     
-    public int AssetId { get; init; }
-    
+    /// <summary>
+    /// Represents a progress unit for a quest objective
+    /// </summary>
+    /// <param name="taskTypeId">The id of the task progressed</param>
+    /// <param name="progressValue">The amount of progress made</param>
+    /// <param name="assetId">The id of the asset related to the progress</param>
     public ObjectiveProgressDto(int taskTypeId, int progressValue, int assetId = -1)
     {
         TaskTypeId = taskTypeId;
         ProgressValue = progressValue;
         AssetId = assetId;
+    }
+
+    public override string ToString() {
+        return $"Objective Progress Dto- Progress: {ProgressValue} on taskId:{TaskTypeId} for asset:{AssetId}";
     }
 }
---
+++ QuestSystemTests/ObjectiveProgressDtoTests.cs
@@ -0,0 +1,38 @@
+﻿using QuestSystem;
+
+namespace QuestSystemTests;
+
+public class ObjectiveProgressDtoTests
+{
+    [Fact]
+    public void CanCreateDto()
+    {
+        var dto1 = new ObjectiveProgressDto(2, 5, 3);
+        var dto2 = new ObjectiveProgressDto(3, 6);
+        
+        Assert.Equal(2, dto1.TaskTypeId);
+        Assert.Equal(5, dto1.ProgressValue);
+        Assert.Equal(3, dto1.AssetId);
+        Assert.Equal(3, dto2.TaskTypeId);
+        Assert.Equal(6, dto2.ProgressValue);
+        Assert.Equal(-1, dto2.AssetId);
+    }
+    
+    [Fact]
+    public void CanCheckEquality()
+    {
+        var dto1 = new ObjectiveProgressDto(2, 5, 3);
+        var dto2 = new ObjectiveProgressDto(2, 5, 3);
+        
+        Assert.Equal(dto1, dto2);
+    }
+    
+    [Fact]
+    public void CanWriteToString()
+    {
+        var dto = new ObjectiveProgressDto(2, 5, 3);
+        var expected = $"Objective Progress Dto- Progress: 5 on taskId:2 for asset:3";
+        
+        Assert.Equal(expected, dto.ToString());
+    }
+}
