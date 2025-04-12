5783560d952c81f917633a8d89d55d3b2da90a81
Fanis <thfanis649@gmail.com>
2025-04-12 14:16:51 +0300

    Added example json to exampleUse

--- ExampleUse/Program.cs
+++ ExampleUse/Program.cs
@@ -1,4 +1,13 @@
 ﻿using ExampleUse;
+using QuestSystem.Parser;
 
-var quest = Example.CreateMultiStageQuest();
-Example.CompleteMultiStageQuest(quest);
+// Code-based Quest Generation
+// var quest = Example.CreateMultiStageQuest();
+// Example.CompleteMultiStageQuest(quest);
+
+// Json-based Quest Generation
+string jsonString = File.ReadAllText("Example.json");
+var questList = QuestParser.LoadFromJson(jsonString);
+Console.WriteLine($"Found {questList.Count} quests in json file");
+Console.WriteLine($"First quest id:{questList[0].Id} title:{questList[0].Title}");
+Console.WriteLine($"First quest id:{questList[1].Id} title:{questList[1].Title}");
